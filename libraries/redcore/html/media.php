57659239c574bc9a675778ddb2f10e6620e3d6b8
Thong Tran <thong@redweb.dk>
2015-07-24 15:58:07 +0700

    Bootflat

--- libraries/redcore/html/media.php
+++ libraries/redcore/html/media.php
@@ -114,9 +114,7 @@ abstract class RHtmlMedia
 			}
 			elseif (self::getFramework() == 'bootstrap3')
 			{
-				// RHelperAsset::load('lib/bootstrap3/css/bootstrap.min.css', 'redcore');
-				// RHelperAsset::load('lib/bootstrap3/css/bootflat.min.css', 'redcore');
-				RHelperAsset::load('component.bs3.min.css', 'redcore');
+				RHelperAsset::load('component-bootflat.bs3.min.css', 'redcore');
 			}
 			elseif (self::getFramework() == 'foundation5')
 			{
