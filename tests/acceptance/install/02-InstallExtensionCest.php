52ad6d63f566cc4c7fb44acc791b4997418ead24
thongredweb <thong@redweb.dk>
2018-01-15 13:25:11 +0700

    [Travis] Fix Webservice Manager check

--- tests/acceptance/install/02-InstallExtensionCest.php
+++ tests/acceptance/install/02-InstallExtensionCest.php
@@ -33,7 +33,7 @@ class InstallExtensionCest
 		$I->click(['xpath' => "//button[contains(@onclick, 'config.apply')]"]);
 		$I->waitForText('Save success', 30, ['id' => 'system-message-container']);
 		$I->amOnPage('administrator/index.php?option=com_redcore&view=webservices');
-		$I->waitForText('Webservice Manager', 30, ['css' => 'H1']);
+		$I->waitForText('Webservice Manager', 30, ['css' => 'h1']);
 		$I->click(['class' => 'lc-not_installed_webservices']);
 		$I->waitForElement(['class' => 'lc-install_all_webservices'], 60);
 		$I->executeJS("javascript:window.scrollBy(0,200);");
